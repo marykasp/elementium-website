837210faffc58052fe65fa555af5c62f0c2a4805
marykasp <kasparian.mary@gmail.com>
2023-11-30 15:43:45 -0600

    chore: remove margin and add gap to menu items

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,13 +8,12 @@ const Navbar = () => {
   return (
     <nav className="w-full flex py-6 justify-between items-center navbar">
       <img src={logo} alt="elementium" className="w-[124px] h-[32px]" />
-      <ul className="list-none sm:flex hidden  items-center ">
+      <ul className="list-none sm:flex hidden justify-between gap-10 items-center ">
         {navLinks.map((nav, index) => (
           <li
             key={nav.id}
-            className={`font-poppins font-normal cursor-pointer text-[16px] text-white ${
-              index === navLinks.length - 1 ? "mr-0" : "mr-10"
-            }`}
+            className={`font-poppins font-normal cursor-pointer text-[16px] text-white 
+            `}
           >
             <a href={`#${nav.id}`}>{nav.title}</a>
           </li>
