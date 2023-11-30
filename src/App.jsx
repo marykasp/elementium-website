bf71190da6656a367e1cc9dd5a24e54de8409416
marykasp <kasparian.mary@gmail.com>
2023-11-30 15:07:55 -0600

    chore: default setup of all components, import into App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,46 @@
+import styles from "./style";
+import {
+  Navbar,
+  Hero,
+  Stats,
+  Testimonials,
+  Clients,
+  Billing,
+  Business,
+  CallToAction,
+  CardDeal,
+} from "./components";
+
 function App() {
   return (
     <div className="bg-primary w-full overflow-hidden">
       {/* wrapper for navbar */}
-      <div></div>
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          {/* navbar */}
+          <Navbar />
+        </div>
+      </div>
+
+      {/* hero section */}
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Hero />
+        </div>
+      </div>
+
+      {/* main section */}
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Stats />
+          <Billing />
+          <Business />
+          <Testimonials />
+          <Clients />
+          <CallToAction />
+          <CardDeal />
+        </div>
+      </div>
     </div>
   );
 }
