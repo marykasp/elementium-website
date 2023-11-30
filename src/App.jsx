6c5a0f191826773947b868b054c6ce174f567d21
marykasp <kasparian.mary@gmail.com>
2023-11-30 14:54:14 -0600

    style: create tailwind constant styles for layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 function App() {
-  return <></>;
+  return (
+    <div className="bg-primary w-full overflow-hidden">
+      {/* wrapper for navbar */}
+      <div></div>
+    </div>
+  );
 }
 
 export default App;
