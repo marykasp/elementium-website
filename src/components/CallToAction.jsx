22b00e29c1d9c4c2b474fcad91e752953451cfbf
marykasp <kasparian.mary@gmail.com>
2023-12-04 14:28:59 -0600

    style: create call to action card

--- src/components/CallToAction.jsx
+++ src/components/CallToAction.jsx
@@ -1,5 +1,25 @@
+import styles from "../style";
+import Button from "./Button";
+
 const CallToAction = () => {
-  return <div>CallToAction</div>;
+  return (
+    <section
+      className={`${styles.marginY} ${styles.padding} ${styles.flexCenter} sm:flex-row flex-col bg-slate-900/80 rounded-sm box-shadow`}
+    >
+      <div className="flex-1 flex-col">
+        <h2 className={styles.heading3}>
+          Ready to Elevate Your Design Experience?
+        </h2>
+        <p className={`max-w-[570px] mt-2 ${styles.paragraph}`}>
+          Elevate your projects, streamline your process, and unleash your
+          creative potential. Get started today!
+        </p>
+      </div>
+      <div className={`${styles.flexCenter} sm:ml-10 ml-0`}>
+        <Button>Try Now!</Button>
+      </div>
+    </section>
+  );
 };
 
 export default CallToAction;
--- src/style.js
+++ src/style.js
@@ -3,6 +3,9 @@ const styles = {
 
   heading2:
     "font-poppins font-semibold xs:text-[48px] text-[40px] text-white xs:leading-[76.8px] leading-[66.8px] w-full",
+  heading3:
+    "font-poppins font-medium xs:text-[32px] text-[24px] text-white xs:leading-[76.8px] leading-[66.8px] w-full",
+
   paragraph:
     "font-poppins font-normal text-dimWhite text-[18px] leading-[30.8px]",
 
