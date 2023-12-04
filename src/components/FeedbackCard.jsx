c706130eed16d67e081eb2f9459b4279fd389ace
marykasp <kasparian.mary@gmail.com>
2023-12-04 14:11:05 -0600

    style: create testimonial feedback card components

--- src/components/FeedbackCard.jsx
+++ src/components/FeedbackCard.jsx
@@ -1,5 +1,29 @@
-const FeedbackCard = () => {
-  return <div>FeedbackCard</div>;
+import { quotes } from "../assets";
+
+const FeedbackCard = ({ id, content, name, title, img }) => {
+  return (
+    <div className="flex justify-between flex-col px-10 py-12 rounded-[4px] max-w-[380px] md:mr-10 sm:mr-5 mr-0 my-5 bg-slate-900 hover:bg-gradient-to-b hover:from-slate-800">
+      <img
+        src={quotes}
+        alt="quotation marks"
+        className="w-[42px] h-[27px] object-contain"
+      />
+      <p className="font-poppins font-normal text-[16px] leading-[22px] text-white my-5">
+        {content}
+      </p>
+      <div className="flex flex-row">
+        <img src={img} alt={name} className="w-[48px] h-[48px] rounded-full" />
+        <div className="flex flex-col ml-4">
+          <h4 className="font-poppins font-semibold text-[18px] text-white leading-[30px]">
+            {name}
+          </h4>
+          <p className="font-poppins font-normal text-[14px] leading-[24px] text-dimWhite">
+            {title}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default FeedbackCard;
--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -3,7 +3,28 @@ import styles from "../style";
 import { feedback } from "../constants";
 
 const Testimonials = () => {
-  return <div>Testimonials</div>;
+  return (
+    <section
+      id="clients"
+      className={`${styles.paddingY} ${styles.flexCenter} flex-col relative`}
+    >
+      {/* title and context */}
+      <div className="w-full flex justify-between items-center md:flex-row flex-col md:mb-16 mb-6 relative z-[1]">
+        <h2 className={styles.heading2}>What people are saying</h2>
+        <div className="w-full md:mt-0 mt-6">
+          <p className={`${styles.paragraph} text-left max-w-[450px]`}>
+            Discover what others are saying about Elementium's transformative
+            impact
+          </p>
+        </div>
+      </div>
+      <div className="flex flex-wrap sm:justify-start justify-center w-full feedback-container relative z-[1]">
+        {feedback.map((feedback) => (
+          <FeedbackCard key={feedback.id} {...feedback} />
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default Testimonials;
