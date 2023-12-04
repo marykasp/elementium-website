0f011bdc04d57ff57ccbd13f792a1f2608f78379
marykasp <kasparian.mary@gmail.com>
2023-12-04 12:49:17 -0600

    style: add stats to bottom of header
    Iterate over and display value and title of each stat

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,5 +1,26 @@
+import styles from "../style";
+import { stats } from "../constants";
+
 const Stats = () => {
-  return <div>Stats</div>;
+  return (
+    <section
+      className={`flex justify-center items-center flex-wrap sm:mb-20 mb-6`}
+    >
+      {stats.map((stat, idx) => (
+        <div
+          key={stat.id}
+          className={`flex-1 justify-start items-center flex m-3`}
+        >
+          <h4 className="font-poppins font-semibold xs:text-[40px] text-[30px] xs:leading-[53px] leading-[43px] text-white">
+            {stat.value}
+          </h4>
+          <p className="font-poppins font-normal xs:text-[20px] text-[15px] xs:leading-[26px] leading-[21px] text-gradient uppercase ml-3">
+            {stat.title}
+          </p>
+        </div>
+      ))}
+    </section>
+  );
 };
 
 export default Stats;
